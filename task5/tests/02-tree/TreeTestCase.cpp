75005a786e40890d7d11bcfcea1b8c38b4080099
nadir <s.nodir9494@gmail.com>
2020-12-17 03:08:20 +0300

    feuture SetUp and TearDown

--- task5/tests/02-tree/TreeTestCase.cpp
+++ task5/tests/02-tree/TreeTestCase.cpp
@@ -2,21 +2,34 @@
 // Created by akhtyamovpavel on 5/1/20.
 //
 
-
 #include "TreeTestCase.h"
 #include "Tree.h"
 
 #include <iostream>
 
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+#include <string>
+#include <fstream>
+
+namespace fs = boost::filesystem;
+
+
 TEST_F(TreeTestCase, test1)
 {
-    ASSERT_TRUE(GetTree(".", false).is_dir);
-    ASSERT_NO_THROW(GetTree("..", false));
-    ASSERT_NO_THROW(GetTree("..", true));
-    ASSERT_GE(GetTree("..", true).children.size(), 0);
-    ASSERT_THROW(GetTree("non-existing-file.txt", false), std::invalid_argument);
-    ASSERT_THROW(GetTree("klasdasdasd.txt", true), std::invalid_argument);
-    FileNode f1 = GetTree(".", true);
-    FileNode f2 = GetTree(".", true);
+    fs::path temp_path = fs::current_path();
+    temp_path /= "temp";
+    std::string temp = temp_path.string(); 
+    ASSERT_TRUE(GetTree(temp, false).is_dir);
+    ASSERT_NO_THROW(GetTree(temp, false));
+    ASSERT_NO_THROW(GetTree(temp, true));
+    ASSERT_GE(GetTree(temp, true).children.size(), 0);
+    ASSERT_EQ(GetTree(temp, false).children.size(), 1);
+    ASSERT_THROW(GetTree(temp + "/non-existing-file.txt", false), std::invalid_argument);
+    ASSERT_THROW(GetTree(temp + "/klasdasdasd.txt", true), std::invalid_argument);
+    FileNode f1 = GetTree(temp, false);
+    FileNode f2 = GetTree(temp, false);
+    FileNode f3 = GetTree(".", false);
     ASSERT_TRUE(f1 == f2);
+    ASSERT_FALSE(f1 == f3);
 }
--- task5/tests/02-tree/TreeTestCase.h
+++ task5/tests/02-tree/TreeTestCase.h
@@ -6,9 +6,30 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include <boost/filesystem.hpp>
 #include <boost/filesystem/path.hpp>
+#include <string>
+#include <fstream>
+
+namespace fs = boost::filesystem;
+
 
 class TreeTestCase : public ::testing::Test {
-};
+protected:
+    
+    virtual void SetUp() {
+        fs::path temp_path = fs::current_path();
+        temp_path /= "temp/file.txt";
+        fs::create_directories(temp_path.parent_path());
+        std::ofstream ofs1(temp_path.string());
+        ofs1 << "text\n";
+        ofs1.close();
+    }
 
+    virtual void TearDown() {
+        fs::path temp_path = fs::current_path();
+        temp_path /= "temp";
+        fs::remove_all(temp_path);
+    }
+};
 
